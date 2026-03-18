878cb0474406ca8eb08521d2c2d146a961e3a6f4
fb <fb@dev.null>
2026-03-18 09:23:40 +0100

    Fix exporter_main export for backward compatibility

--- xmrig_exporter/__init__.py
+++ xmrig_exporter/__init__.py
@@ -1,4 +1,8 @@
 from .collector import XmrigCollector
 from .proxy_collector import XmrigProxyCollector
+from .exporter import main
 
 __version__ = "1.1.0"
+
+# Backward compatibility
+exporter_main = main
